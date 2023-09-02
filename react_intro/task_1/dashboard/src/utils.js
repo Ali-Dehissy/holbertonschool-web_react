e0923c46e7022faad6aa347db1a580ead184af12
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-09-02 16:25:44 +0100

    export func

---
+++ react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,5 @@
+// getFullYear that will return the current year
+export const getFullYear = () => new Date().getFullYear();
+
+export const getFooterCopy = (isIndex) =>
+  isIndex ? 'Holberton School' : 'Holberton School Main Dashboard';
