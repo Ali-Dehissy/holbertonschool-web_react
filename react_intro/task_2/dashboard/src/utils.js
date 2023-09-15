6a904a3b7b9c4f8573ad18739ccd0f6b8e1e868c
Ali-Dehissy <ali1998dehissy@gmail.com>
2023-09-15 18:07:45 +0200

    update

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -1,10 +1,10 @@
-// getFullYear fucntion that will return the current year
-export const getFullYear = () => new Date().getFullYear();
-
-// getFooterCopy function  that accepts one argument isIndex(boolean). 
-export const getFooterCopy = (isIndex) =>
-  isIndex ? 'Holberton School' : 'Holberton School Main Dashboard';
-
-// getLatestNotification function that accepts one argument isIndex(boolean). 
-export const getLatestNotification = () => 
-  '<strong>Urgent requirement</strong> - complete by EOD';
+export function getFullYear() {
+    return new Date().getFullYear()
+}
+export function getFooterCopy(isIndex) {
+    if (isIndex === true){
+        return ("Holberton School");
+    }else{
+        return ("Holberton School main dashboard");
+    }
+}
