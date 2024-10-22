d032abba23d4bc36314565f1ca1dd310859e5e7b
Ali-Dehissy <ali1998dehissy@gmail.com>
2024-10-22 11:57:00 +0200

    update

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -1,50 +1,54 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './App.css';
-import Header from '../Header/Header';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-import Notifications from '../Notifications/Notifications';
-import CourseList from '../CourseList/CourseList';
-import { getLatestNotification } from '../utils/utils';
+import React, { PureComponent } from 'react'
+import Notifications from '../Notifications/Notifications'
+import { getLatestNotification } from '../utils/utils'
+import Login from '../Login/Login'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import CourseList from '../CourseList/CourseList'
+import propTypes from 'prop-types'
 
-class App extends React.Component {
-  static propTypes = {
-    isLoggedIn: PropTypes.bool,
-  };
 
-  static defaultProps = {
-    isLoggedIn: false,
-  };
+// implement class components
+class App extends PureComponent {
+	render() {
+		// assign props to local variables
+		const { isLoggedIn } = this.props;
 
-  render() {
-    const isIndex = true;
-    const listCourses = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 }
-    ];
-    const listNotifications = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
-    ];
+		const listCourses = [
+			{ id: 1, name: 'ES6', credit: '60' },
+			{ id: 2, name: 'Webpack', credit: '20' },
+			{ id: 3, name: 'React', credit: '40' }
+		]
+		
+		const listNotifications = [
+			{ id: 1, type: "default", value: "New course available" },
+			{ id: 2, type: "urgent", value: "New resume available" },
+			{ id: 3, html: { __html: getLatestNotification() }, type: "urgent" }
+		]
+		
 
-    return (
-      <>
-        <Notifications listNotifications={listNotifications} />
-        <div className="App">
-          <Header />
-            {this.props.isLoggedIn ? (
-              <CourseList listCourses={listCourses} />
-            ) : (
-              <Login />
-            )}
-          <Footer isIndex={isIndex} />
-        </div>
-      </>
-    )
-  }
+		return (
+			<div className="App">
+				<Notifications listNotifications={listNotifications} />
+				<Header />
+				<div className="App-body">
+					{this.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+				</div>
+				<div className="App-footer">
+					<Footer />
+				</div>
+			</div>
+		)
+	}
 }
 
-export default App;
+
+App.defaultProps = {
+	isLoggedIn: false,
+}
+
+App.propTypes = {
+	isLoggedIn: propTypes.bool,
+}
+
+export default App
